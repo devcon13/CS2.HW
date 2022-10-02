674ac92b8c4b03bf329c83998a9681329d9e7d50
Devon <devon_connerly24@milton.edu>
2022-10-02 10:34:17 -0400

    HW 10/2/2022: Static methods & overloading

--- src/Food.java
+++ src/Food.java
@@ -1,11 +1,26 @@
 public class Food {
     private int taste = 3;
+    public static String food;
 
     public int eat(){
         return taste;
     }
-
     public void cook(int t){
         taste = t;
+    }
+
+    public static void foodType(String name){
+        food = name;
+    }
+    public static void rating(int x){
+        System.out.println(x+" stars");
+    }
+    public static void rating(boolean yORn){
+        if(yORn == true){
+            System.out.println("yay!");
+        }
+        else{
+            System.out.println("nay");
+        }
     }
 }
--- src/RunMyProgram.java
+++ src/RunMyProgram.java
@@ -6,5 +6,12 @@ public class RunMyProgram {
         System.out.println(dinner.eat());
         dinner.cook(10);
         System.out.println(dinner.eat());
+
+        // Static methods/overloading, 10/2/2022
+        Food.foodType("sushi");
+        System.out.println(Food.food);
+        Food.rating(5);
+        Food.rating(true);
+
     }
 }
